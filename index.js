8f0f2391de6505adb9f852d5ce59e6e04dfdc445
Thanos Korakas <seekerx@192.168.1.4>
2017-09-09 19:44:19 +0300

    Initial commit

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express  = require('express');
+const express = require('express');
 const bodyParser = require('body-parser');
 const axios = require('axios');
 const PAGE_ACCESS_TOKEN = 'access token';
@@ -7,7 +7,12 @@ const app = express();
 // Use body-parser middleware.
 app.use(bodyParser.json());
 
-// Validate Facebook webhook.
+// SocialNerds data.
+const socialNerdVideos = require('./videos');
+
+/**
+ * Validate Facebook webhook.
+ */
 app.get('/webhook', function (req, res) {
     if (req.query['hub.verify_token'] === 'SocialNerds') {
         res.send(req.query['hub.challenge']);
@@ -16,7 +21,9 @@ app.get('/webhook', function (req, res) {
     }
 });
 
-// Receiver requests. from Facebook.
+/**
+ * Receive requests from Facebook.
+ */
 app.post('/webhook', function (req, res) {
     const data = req.body;
 
@@ -24,14 +31,10 @@ app.post('/webhook', function (req, res) {
     if (data.object === 'page') {
 
         // Iterate over each entry - there may be multiple if batched
-        data.entry.forEach(function(entry) {
-            var pageID = entry.id;
-            var timeOfEvent = entry.time;
-
+        data.entry.forEach(function (entry) {
             // Iterate over each messaging event
-            entry.messaging.forEach(function(event) {
+            entry.messaging.forEach(function (event) {
                 if (event.message) {
-                    console.log(event.message);
                     receivedMessage(event);
                 } else {
                     console.log("Webhook received unknown event: ", event);
@@ -43,60 +46,125 @@ app.post('/webhook', function (req, res) {
     }
 });
 
+/**
+ * Handle received message.
+ *
+ * @param event
+ */
 function receivedMessage(event) {
-    // Putting a stub for now, we'll expand it in the following steps
-    console.log("Message data: ", event.message);
-    var senderID = event.sender.id;
-    var recipientID = event.recipient.id;
-    var timeOfMessage = event.timestamp;
-    var message = event.message;
+    const senderID = event.sender.id;
+    const recipientID = event.recipient.id;
+    const timeOfMessage = event.timestamp;
+    const message = event.message;
 
     console.log("Received message for user %d and page %d at %d with message:",
         senderID, recipientID, timeOfMessage);
-    console.log(JSON.stringify(message));
+    const messageText = message.text;
 
-    var messageId = message.mid;
-
-    var messageText = message.text;
-    var messageAttachments = message.attachments;
-
+    let messageData = '';
     if (messageText) {
-
-        // If we receive a text message, check to see if it matches a keyword
-        // and send back the example. Otherwise, just echo the text we received.
         switch (messageText) {
-            case 'generic':
-                sendGenericMessage(senderID);
+            case 'hello':
+                messageData = sendHelloMessage(senderID);
                 break;
-
             default:
-                sendTextMessage(senderID, messageText);
+                messageData = sendSearchMessage(senderID, messageText);
         }
-    } else if (messageAttachments) {
-        sendTextMessage(senderID, "Message with attachment received");
     }
-}
-function sendTextMessage(recipientId, messageText) {
-    var messageData = {
-        recipient: {
-            id: recipientId
-        },
-        message: {
-            text: 'Hello nerd'
-        }
-    };
 
     callSendAPI(messageData);
 }
 
+/**
+ * Respond to 'hello' message or the message passed as param.
+ *
+ * @param recipientId
+ * @param message(optional)
+ * @return {{recipient: {id: *}, message: {text: (string|string)}}}
+ */
+function sendHelloMessage(recipientId, message = '') {
+    return messageData = {
+        recipient: {
+            id: recipientId
+        },
+        message: {
+            text: message.trim() || 'Hello nerd, send something to search on our videos!'
+        }
+    };
+}
+
+/**
+ * Search on SocialNerds videos array and return the videos or a sorry message.
+ *
+ * @param recipientId
+ * @param messageText
+ * @return {string}
+ */
+function sendSearchMessage(recipientId, messageText) {
+    const match = new RegExp(messageText, 'i');
+    const videos = socialNerdVideos.filter(video => match.test(video.title));
+    let messageData = '';
+
+    if (videos.length > 0) {
+        messageData = {
+            recipient: {
+                id: recipientId
+            },
+            message: {
+                attachment: {
+                    type: "template",
+                    payload: {
+                        template_type: "generic",
+                        elements: buildMessage(videos)
+                    }
+                }
+            }
+
+        };
+    } else {
+        messageData = sendHelloMessage(recipientId, 'Sorry, we are lazy too!');
+    }
+
+    return messageData;
+}
+
+/**
+ * Map matched videos to Facebook template response.
+ *
+ * @param videos
+ * @return {Array}
+ */
+function buildMessage(videos) {
+    return videos.map(video => {
+        return {
+            title: video.title,
+            subtitle: video.description,
+            item_url: video.url,
+            image_url: 'https://scontent.fath3-1.fna.fbcdn.net/v/t1.0-9/18058096_1685426578426826_1338045697125253103_n.png?oh=d446a0ce4807c9634caa8a5f97960028&oe=5A5A0BA0',
+            buttons: [{
+                type: "web_url",
+                url: video.url,
+                title: "Watch video"
+            }],
+        }
+    });
+}
+
+/**
+ * Send the response to Facebook.
+ *
+ * @param messageData
+ */
 function callSendAPI(messageData) {
     axios.post(`https://graph.facebook.com/v2.6/me/messages?access_token=${PAGE_ACCESS_TOKEN}`, messageData)
         .then(res => {
-            console.log(res);
+            console.log('Done')
         })
-        .catch(error => {});
+        .catch(error => {
+        });
 }
 
+// Start the server.
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
 });
---
+++ videos.js
@@ -0,0 +1,108 @@
+module.exports = [
+    {
+        title: 'React and MobX',
+        description: 'Θα μιλήσουμε για React και πως μπορείς να χρησιμοποιήσεις.',
+        url: 'https://www.youtube.com/watch?v=zaXrOtpOn7U'
+    },
+    {
+        title: 'Agile Dev',
+        description: 'Θα μιλήσουμε για development βασισμένο σε Agile methodology',
+        url: 'https://www.youtube.com/watch?v=9QhUmYTS_Uc'
+    },
+    {
+        title: 'REST',
+        description: 'Θα μιλήσουμε για REST web services',
+        url: 'https://www.youtube.com/watch?v=WAIZ6fq7uMU'
+    },
+    {
+        title: 'Microservices & SSO',
+        description: 'Θα μιλήσουμε για τη σημαντικότητα του να χωρίζεις την εφαρμογή σου σε microservices και πως τα συνδέεις με Single Sign On server.',
+        url: 'https://www.youtube.com/watch?v=FKeZk4hr9j0',
+    },
+    {
+        title: 'Dependency injection container',
+        description: 'Θα μιλήσουμε για το πως δουλεύει και γιατί χρειάζεται Dependency Injection Container η εφαρμογή σου. Θα δούμε το Symfony framework και to DependencyInjection Component που παρέχει για real life παραδείγματα',
+        url: 'https://www.youtube.com/watch?v=qL3hge1ldts',
+    },
+    {
+        title: 'Δημιουργία custom WordPress template με Bootstrap',
+        description: 'Theming πάνω σε Bootstrap για την δημιουργία custom template σε WordPress.',
+        url: 'https://youtube.com/socialnerdsgr',
+    },
+    {
+        title: 'Pac-Man',
+        description: 'Θα φτιάξουμε live ένα παιχνίδι σε HTML5 (canvas) και JavaScript που ευελπιστούμε να μοιάζει με Pac-Man.',
+        url: 'https://www.youtube.com/watch?v=n8n1CpqVgtc&',
+    },
+    {
+        title: 'Pac-Man II',
+        description: 'Θα συνεχίσουμε το development του Pac-Man σε HTML5. Θα επικεντρωθούμε σε collision detection, AI κτλ.',
+        url: 'https://www.youtube.com/watch?v=p5gZZ8wi-S8',
+    },
+    {
+        title: 'Google Webmasters, Analytics, DNS, SMTP',
+        description: 'Τι χρειάζεται να ξέρει ένας web developer αφού φτιάξει το site και πριν το παραδώσει στον πελάτη.',
+        url: 'https://www.youtube.com/watch?v=f1E6Tk8YwaU',
+    },
+    {
+        title: 'Native v Hybrid, Dawn of Mobile Development',
+        description: 'Θα μιλήσουμε casually για native mobile development με Java και Swift σε Android και iOS αντίστοιχα. Θα συζητήσουμε για τις διαφορές με React Native, Ionic, NativeScript, Cordova.',
+        url: 'https://www.youtube.com/watch?v=HIDe2-NTtvw',
+    },
+    {
+        title: 'Ionic app με League of Legends API',
+        description: 'Δημιουργία Ionic mobile εφαρμογής σε συνδιασμό με το REST API του League of Legends.',
+        url: 'https://www.youtube.com/watch?v=1XNJfiPNjSM',
+    },
+    {
+        title: 'Website Performance',
+        description: 'Θα μιλήσουμε για το πως μπορούμε να αυξήσουμε την ταχύτητα σε ένα website ή web εφαρμογή.',
+        url: 'https://www.youtube.com/watch?v=Zq18tZbo-LI',
+    },
+    {
+        title: 'Docker',
+        description: 'Θα μιλήσουμε για το πως μπορούμε να εγκαταστήσουμε Docker και γιατί να το χρησιμοποιήσουμε.',
+        url: 'https://www.youtube.com/watch?v=WSvnD_IeFWk',
+    },
+    {
+        title: 'Πως αρχίζουμε με Laravel',
+        description: 'Θα μιλήσουμε για τις δυνατότητες του Laravel και πως μπορούμε να ξεκινήσουμε τη δημιουργία ενός website ή μιας web εφαρμογής.',
+        url: 'https://www.youtube.com/watch?v=jrYvAS7tmkg',
+    },
+    {
+        title: 'SEO & web marketing',
+        description: 'Θα μιλήσουμε για Search Engine Optimization και web marketing από την πλευρά του προγραμματιστή και όχι μόνο. Θα αναφαίρουμε Google Analytics, Webmasters, PageSpeed & διάφορα άλλα tools που μας βοηθούν να προβάλουμε τη σελίδα μας.',
+        url: 'https://www.youtube.com/watch?v=WavfbgLcJg8',
+    },
+    {
+        title: 'Large scale hosting',
+        description: 'Παρουσίαση πάνω σε Google Cloud και γενικότερα σχολιασμός για large scale hosting.',
+        url: 'https://www.youtube.com/watch?v=G2vwIOpbWzo',
+    },
+    {
+        title: 'ECMAScript 6',
+        description: 'Παρουσίαση της ECMAScript 6 σε Hangouts session.',
+        url: 'https://www.youtube.com/watch?v=Q177R8692Xc',
+    },
+    {
+        title: 'React series',
+        description: 'React series playlist.',
+        url: 'https://www.youtube.com/watch?v=uCFFS9jxlck&list=PLuswtImoIwq9ZxO6iUKu1K00iKakWfiAL',
+    },
+    {
+        title: 'Chat εφαρμογή με AngularJS & Socket.io',
+        description: 'Δημιουργία chat εφαρμογής με AngularJS, Node.js και Socket.io.',
+        url: 'https://www.youtube.com/watch?v=2UpZl2uKuVw',
+    },
+    {
+        title: 'Theming με Twig & stuff',
+        description: 'Θα μιλήσουμε για theming χρησιμοποιώντας Twig, ένα από τα components του Symfony, responsive design, User eXperience και γενικά διάφορα εργαλεία που βοηθούν.',
+        url: 'https://www.youtube.com/watch?v=WnceNsaXKtU',
+    },
+    {
+        title: 'Ruby on Rails',
+        description: 'Θα μας αναλύσει ο Χρήστος πως ξεκινάμε με Ruby on Rails για να φτιάξουμε μία τυπική εφαρμογή.',
+        url: 'https://www.youtube.com/watch?v=frzYPhW6YLU',
+    }
+];
+
